c2ad92162f6eb6c8bce91b1e6ea4e99074b3ac57
Maxime Beauchemin <maximebeauchemin@gmail.com>
2015-06-10 23:09:22 -0700

    A simple plugin system for Airflow

--- airflow/__init__.py
+++ airflow/__init__.py
@@ -1,11 +1,11 @@
-__version__ = "1.0.1"
-
 """
 Authentication is implemented using flask_login and different environments can
 implement their own login mechanisms by providing an `airflow_login` module
 in their PYTHONPATH. airflow_login should be based off the
 `airflow.www.login`
 """
+__version__ = "1.0.1"
+
 import logging
 from airflow.configuration import conf
 from airflow.models import DAG
@@ -20,3 +20,8 @@ if conf.getboolean('webserver', 'AUTHENTICATE'):
             "authenticate is set to True in airflow.cfg, "
             "but airflow_login failed to import")
 
+
+class PluginView(object):
+    @classmethod
+    def get_views(self):
+        raise NotImplemented("Derive me")
--- airflow/executors/__init__.py
+++ airflow/executors/__init__.py
@@ -1,6 +1,7 @@
 import logging
 
 from airflow.configuration import conf
+from airflow.executors.base_executor import BaseExecutor
 from airflow.executors.local_executor import LocalExecutor
 from airflow.executors.celery_executor import CeleryExecutor
 from airflow.executors.sequential_executor import SequentialExecutor
@@ -14,7 +15,14 @@ elif _EXECUTOR == 'CeleryExecutor':
     DEFAULT_EXECUTOR = CeleryExecutor()
 elif _EXECUTOR == 'SequentialExecutor':
     DEFAULT_EXECUTOR = SequentialExecutor()
+elif _EXECUTOR in globals():
+    DEFAULT_EXECUTOR = globals()[_EXECUTOR]
 else:
+    # Loading plugins
+    from airflow.plugins_manager import get_plugins
+    executor_plugins = {}
+    for _plugin in get_plugins(BaseExecutor):
+        globals()[_plugin.__name__] = _plugin
     raise AirflowException("Executor {0} not supported.".format(_EXECUTOR))
 
 logging.info("Using executor " + _EXECUTOR)
